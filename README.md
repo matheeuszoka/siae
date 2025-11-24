d29eb09cce4b3f483a17e2cd65fbc56bc02f2980
Matheus Gabriel <matheuschampan@hotmail.com>
2025-11-24 19:24:35 -0300

    Inicio SIAE

---
+++ README.md
@@ -0,0 +1,2 @@
+# siae
+Sistema Integrado de Arquivos Eletrônicos (SIAE) para tramitação, organização e gestão de documentos em instituições públicas. Desenvolvido com Spring Boot, React, PostgreSQL e MinIO, oferece controle de fluxo documental entre setores, auditoria completa.
---
+++ back-end/src/main/java/br/com/mgctecnologia/siae/api/SiaeApplication.java
@@ -0,0 +1,13 @@
+package br.com.mgctecnologia.siae.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiaeApplication.class, args);
+	}
+
+}
