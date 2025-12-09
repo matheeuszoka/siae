ab6b6584744dcde66b3aeff3b31ebfbaa9df4e8b
Matheus Gabriel <matheuschampan@hotmail.com>
2025-12-08 22:44:41 -0300

    Start inicial, projeto utilizando spring, postgresql, auditoria com hibernat envers, minIO para a persistencias de arquivos,pdfbox, itextpdf e bouncycastle para a assinatura de pdfs. Além de todas essas ferramentas, estou utilizando lombok para melhor a identação do código com o @data e @noargsconstructor e @allargsconstructor.

--- back-end/src/main/java/br/com/mgctecnologia/siae/api/SiaeApplication.java
+++ back-end/src/main/java/br/com/mgctecnologia/siae/api/SiaeApplication.java
@@ -2,8 +2,10 @@ package br.com.mgctecnologia.siae.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SiaeApplication {
 
 	public static void main(String[] args) {
